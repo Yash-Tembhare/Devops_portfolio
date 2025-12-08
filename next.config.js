77bee102f1820c8c628d46eac45aeb34c7af70b3
yash-258 <yashtembhare2025@gmail.com>
2025-12-08 18:57:09 +0530

    Update config & workflow for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: 'export',             // static export
     images: {
-        unoptimized: true,
+        unoptimized: true,        // GitHub Pages me image optimization off
     },
-    trailingSlash: true,
+    trailingSlash: true,          // ensures URLs end with slash
+    basePath: '/Devops_portfolio',        // repo name (case sensitive)
+    assetPrefix: '/Devops_portfolio/',    // static assets path
+    experimental: {
+        metadataBase: new URL('https://Yash-Tembhare.github.io/Devops_portfolio/'), // metadata warning fix
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
